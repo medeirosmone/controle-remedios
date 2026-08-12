155db9331ed7e45c04158e45a0acebe802a07577
Medeiros <medeirosmone8@gmail.com>
2026-08-12 12:48:15 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register(${import.meta.env.BASE_URL}sw.js)
+      .then(() => {
+        console.log("Service Worker registrado com sucesso!");
+      })
+      .catch((error) => {
+        console.error("Erro ao registrar Service Worker:", error);
+      });
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
